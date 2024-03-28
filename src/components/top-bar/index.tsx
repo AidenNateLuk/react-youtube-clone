b46ef636a27183c286da5eb7814ecdc0ca625408
AidenNateLuk <idanathanieluk@gmail.com>
2024-03-28 16:50:30 +0200

    setting up the top section of  the Headbar

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import "./styles.scss";
+import LogoWhite from "../../assets/LogoWhite.svg";
+import SearchIcon from "@mui/icons-material/Search";
 
 function TopBar() {
-  return <div className="top-bar">eee</div>;
+  const [isSearching, setIsSearching] = useState(false);
+  function handleSearch() {
+    setIsSearching(!isSearching);
+  }
+  return (
+    <div className="top-bar">
+      <img className="logo" src={LogoWhite} alt="Logo" />
+      <input
+        type="text"
+        name="search"
+        id=""
+        className={isSearching ? "open" : ""}
+      />
+      <div className="top-r">
+        <SearchIcon onClick={() => handleSearch()} />
+        <div className="profile-icon"></div>
+      </div>
+    </div>
+  );
 }
 
 export default TopBar;
