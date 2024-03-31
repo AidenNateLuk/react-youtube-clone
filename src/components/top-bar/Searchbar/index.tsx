f53e612a5a519719bb29bb786d53979595888812
AidenNateLuk <idanathanieluk@gmail.com>
2024-03-31 13:24:47 +0300

    styled the searchar

--- src/components/top-bar/Searchbar/index.tsx
+++ src/components/top-bar/Searchbar/index.tsx
@@ -1,22 +1,23 @@
 import SearchIcon from "@mui/icons-material/Search";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import MicIcon from "@mui/icons-material/Mic";
+import "./styles.scss";
 interface SearchbarProps {
   handleSearch: () => void;
 }
 const Searchbar: React.FC<SearchbarProps> = ({ handleSearch }) => {
   return (
     <div className="search__container">
-      <ArrowBackIcon onClick={() => handleSearch()} />
+      <ArrowBackIcon onClick={() => handleSearch()} className="arrow__icon" />
       <div className="search__bar">
         <input
           type="text"
           className="search__bar__input"
           placeholder="Search Youtube"
         />
-        <SearchIcon />
+        <SearchIcon className="search__icon" />
       </div>
-      <MicIcon />
+      <MicIcon className="mic__icon" />
     </div>
   );
 };
--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -8,9 +8,10 @@ interface TopBarProps {
 
 const TopBar: React.FC<TopBarProps> = ({ handleSearch }) => {
   return (
-    <div className="top-bar">
-      <img className="logo" src={LogoWhite} alt="Logo" />
-
+    <div className="top-bar-top">
+      <div className="logo-container">
+        <img className="logo" src={LogoWhite} alt="Logo" />
+      </div>
       <div className="top-r">
         <SearchIcon onClick={() => handleSearch()} />
         <div className="profile-icon"></div>
