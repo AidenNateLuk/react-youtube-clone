725c3b4d1bba7b6af62db13408ba5278fb534f56
AidenNateLuk <idanathanieluk@gmail.com>
2024-04-25 16:01:49 +0300

    initialized bottom navigationbar

---
+++ src/components/navigation/BottomBar/styles.scss
@@ -0,0 +1,5 @@
+// import SlideshowIcon from "@mui/icons-material/Slideshow";
+// import HomeIcon from "@mui/icons-material/Home";
+// import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+// import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+// import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
